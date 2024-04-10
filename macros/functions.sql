4d738ce5464be4a2ab84e0d7aa7d8f7e68bcefec
Jack De Spirlet <jackdespirlet@gmail.com>
2024-04-10 19:09:45 +0000

    creating macro margin percent

--- macros/functions.sql
+++ macros/functions.sql
@@ -1,3 +1,3 @@
-{% macro margin_percent(revenue, purchase_cost) %}
-   ROUND((margin_percent = revenue - purchase_cost),2)
+{% macro margin_percent(revenue, margin) %}
+   ROUND(((s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2)) / revenue),2)
 {% endmacro %}
