ad8785e19ad6309bbfa8bcdaf7451db36a05bf39
Jack De Spirlet <jackdespirlet@gmail.com>
2024-04-10 14:08:43 +0000

    creating inter for finance/marketing

--- models/intermediate/finance_campaigns_month.sql
+++ models/intermediate/finance_campaigns_month.sql
@@ -1,8 +1,8 @@
 SELECT
-    {{ yearmonth("year", "month") }}
-    ,SUM(ads_margin)
-    ,SUM(average_basket)
-    ,SUM(operational_margin)
-    ,SUM(ads_cost)
+    FORMAT_DATE('%Y-%m', date_date) AS year_month
+    ,SUM(ads_margin) AS ads_margin
+    ,ROUND(SUM(average_basket),2) AS average_basket
+    ,SUM(operational_margin) AS operational_margin
+    ,SUM(ads_cost) AS ds_cost
 FROM {{ref("finance_campaigns_day")}}
-GROUP BY year,month
+GROUP BY year_month
